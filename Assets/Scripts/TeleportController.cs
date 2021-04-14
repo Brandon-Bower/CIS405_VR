120293887a3d67c1166932b178f3ce3f3a5e73c4
Brandon <BrandonKBower@gmail.com>
2021-04-13 17:10:16 -0700

    Added basic teleportation

---
+++ Assets/Scripts/TeleportController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Events;
+
+// Code from UNITY XR | ACTION BASED TELEPORTING! tutorial
+// https://www.youtube.com/watch?v=wGvh7Suo1h4&list=WL&index=25&t=370s
+
+public class TeleportController : MonoBehaviour
+{
+    public GameObject baseControllerGameObject;
+    public GameObject teleportationGameObject;
+
+    public InputActionReference teleportationActivationReference;
+
+    [Space]
+    public UnityEvent onTeleportActivate;
+    public UnityEvent onTeleportCancel;
+
+    private void Start()
+    {
+        teleportationActivationReference.action.performed += TeleportModeActivate;
+        teleportationActivationReference.action.canceled += TeleportModeCancel;
+    }
+
+    private void TeleportModeActivate(InputAction.CallbackContext obj) => onTeleportActivate.Invoke();
+
+    private void TeleportModeCancel(InputAction.CallbackContext obj) => Invoke("DeactivateTeleporter", .1f);
+
+    void DeactivateTeleporter() => onTeleportCancel.Invoke();
+
+}
